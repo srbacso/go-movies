f1f3bd889cc1f7c8fdfc8a573955be9770c389a7
Stephen Bacso <stephen.bacso@gmail.com>
2022-08-29 11:00:21 -0400

    Added movie edit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,78 @@
-import React, { Component, Fragment } from 'react';
-import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} from 'react-router-dom';
-import Movies from './components/Movies';
-import Admin from './components/Admin';
-import Home from './components/Home';
-import OneMovie from './components/OneMovie';
-import Genres from './components/Genres';
-import OneGenre from './components/OneGenre';
+import React, { Component, Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import Movies from "./components/Movies";
+import Admin from "./components/Admin";
+import Home from "./components/Home";
+import OneMovie from "./components/OneMovie";
+import Genres from "./components/Genres";
+import OneGenre from "./components/OneGenre";
+import EditMovie from "./components/EditMovie";
 
 export default function App() {
   return (
     <Router>
-    <div className="container">
-
-      <div className="row">
-        <h1 className="mt-3">
-          Go Watch a Movie!
-        </h1>
-        <hr className="mb-3"></hr>
-      </div>
-
-      <div className="row">
-        <div className="col-md-2">
-          <nav>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/movies">Movies</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/genres">Genres</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/admin">Manage Catalogue</Link>
-              </li>
-            </ul>
-          </nav>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+          <hr className="mb-3"></hr>
         </div>
 
-        <div className="col-md-10">
-          <Switch>
-            
-            <Route path="/movies/:id" component={OneMovie} />
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/genres">Genres</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin/movie/0">Add movie</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
-            <Route path="/movies">
-              <Movies />
-            </Route>
+          <div className="col-md-10">
+            <Switch>
+              <Route path="/movies/:id" component={OneMovie} />
 
-            <Route path="/genre/:id" component={OneGenre} />
+              <Route path="/movies">
+                <Movies />
+              </Route>
 
-            <Route exact path="/genres">
-              <Genres />
-            </Route>
+              <Route path="/genre/:id" component={OneGenre} />
 
-            <Route path="/admin">
-              <Admin />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
+              <Route exact path="/genres">
+                <Genres />
+              </Route>
+
+              <Route path="/admin/movie/:id" component={EditMovie} />
+
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
     </Router>
   );
 }
