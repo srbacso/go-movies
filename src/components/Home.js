16f68d6a6eefddfa3a4c82ae01ba32f73137b376
Stephen Bacso <stephen.bacso@gmail.com>
2022-08-30 15:55:56 -0400

    Added authorization

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 export default class Home extends Component {
-
-    render() {
-        return (
-            <h2>This is the home page</h2>
-        );
-    }
+  render() {
+    return <h2>This is the home page</h2>;
+  }
 }
